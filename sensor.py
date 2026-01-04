74ea1d84e0a26fa350090d67547487fc79a92171
samcobb92-pixel <samcobb92@gmail.com>
2026-01-04 15:33:20 +0000

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -6,21 +6,20 @@ from esphome.const import (
     CONF_PRESSURE,
     CONF_HUMIDITY,
     CONF_GAS_RESISTANCE,
-    CONF_PARENT
+    CONF_PARENT,
 )
 
 from . import BME690Component
 
 DEPENDENCIES = ["bme690"]
 
-# define the schema
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_PARENT): cv.use_id(BME690Component),
     cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(unit_of_measurement="°C", accuracy_decimals=2),
     cv.Optional(CONF_PRESSURE): sensor.sensor_schema(unit_of_measurement="hPa", accuracy_decimals=1),
     cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(unit_of_measurement="%", accuracy_decimals=1),
     cv.Optional(CONF_GAS_RESISTANCE): sensor.sensor_schema(unit_of_measurement="Ω", accuracy_decimals=0),
-}).extend(sensor.PLATFORM_SCHEMA)  # must extend PLATFORM_SCHEMA
+}).extend(sensor.PLATFORM_SCHEMA)
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_PARENT])
