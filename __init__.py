4730f1c4c92e266b797c1590619e85cdb2d02e90
samcobb92-pixel <samcobb92@gmail.com>
2026-01-04 15:20:19 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
+from esphome.const import CONF_ID
 from esphome.const import (
     CONF_TEMPERATURE,
     CONF_PRESSURE,
@@ -14,25 +15,16 @@ bme690_ns = cg.esphome_ns.namespace("bme690")
 BME690Sensor = bme690_ns.class_("BME690Component", cg.PollingComponent, i2c.I2CDevice)
 
 CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-                unit_of_measurement="°C", accuracy_decimals=2
-            ),
-            cv.Optional(CONF_PRESSURE): sensor.sensor_schema(
-                unit_of_measurement="hPa", accuracy_decimals=1
-            ),
-            cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(
-                unit_of_measurement="%", accuracy_decimals=1
-            ),
-            cv.Optional(CONF_GAS_RESISTANCE): sensor.sensor_schema(
-                unit_of_measurement="Ω", accuracy_decimals=0
-            ),
-        }
-    )
+    cv.Schema({
+        cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(unit_of_measurement="°C", accuracy_decimals=2),
+        cv.Optional(CONF_PRESSURE): sensor.sensor_schema(unit_of_measurement="hPa", accuracy_decimals=1),
+        cv.Optional(CONF_HUMIDITY): sensor.sensor_schema(unit_of_measurement="%", accuracy_decimals=1),
+        cv.Optional(CONF_GAS_RESISTANCE): sensor.sensor_schema(unit_of_measurement="Ω", accuracy_decimals=0),
+    })
     .extend(i2c.i2c_device_schema(0x76))
-    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.polling_component_schema("60s"))  # ← adds 'id'
 )
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
@@ -54,3 +46,4 @@ async def to_code(config):
     if CONF_GAS_RESISTANCE in config:
         sens = await sensor.new_sensor(config[CONF_GAS_RESISTANCE])
         cg.add(var.set_gas_sensor(sens))
+
